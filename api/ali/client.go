5390f1cdd11034312c1b61cca30400c25abcbf3b
lion <921405104@qq.com>
2025-07-22 11:45:19 +0800

    feat: 阿里文本排序封装

--- api/ali/client.go
+++ api/ali/client.go
@@ -45,3 +45,61 @@ func (c *Client) CreateEmbeddings(req EmbeddingRequest) (EmbeddingResponse, erro
 	err = common.HttpDecodeResponse(responseRaw, &result)
 	return result, err
 }
+
+type (
+	CreateRerankReq struct {
+		Model      string     `json:"model"`
+		Input      Input      `json:"input"`
+		Parameters Parameters `json:"parameters"`
+	}
+	Input struct {
+		Query     string   `json:"query"`
+		Documents []string `json:"documents"`
+	}
+	Parameters struct {
+		ReturnDocuments bool `json:"return_documents"`
+		TopN            int  `json:"top_n"`
+	}
+	CreateRerankRes struct {
+		StatusCode int    `json:"status_code"`
+		RequestId  string `json:"request_id"`
+		Code       string `json:"code"`
+		Message    string `json:"message"`
+		Output     struct {
+			Results []Result `json:"results"`
+		} `json:"output"`
+		Usage struct {
+			TotalTokens int `json:"total_tokens"`
+		} `json:"usage"`
+	}
+	Result struct {
+		Index          int     `json:"index"`
+		RelevanceScore float64 `json:"relevance_score"`
+	}
+)
+
+func (r *CreateRerankReq) validate() error {
+	return nil
+}
+
+func (c *Client) CreateRerank(req *CreateRerankReq) (*CreateRerankRes, error) {
+	result := &CreateRerankRes{}
+	if err := req.validate(); err != nil {
+		return result, err
+	}
+	url := c.EndPoint + "/api/v1/services/rerank/text-rerank/text-rerank"
+	headers := []common.Header{
+		{Key: "Authorization", Value: "Bearer " + c.APIKey},
+		{Key: "Context-Type", Value: "application/json"},
+	}
+	responseRaw, err := common.HttpPost(url, headers, nil, req)
+	if err != nil {
+		return result, err
+	}
+	err = common.HttpCheckError(responseRaw, &ErrorResponse{})
+	if err != nil {
+		return result, err
+	}
+	err = common.HttpDecodeResponse(responseRaw, &result)
+	return result, err
+}
